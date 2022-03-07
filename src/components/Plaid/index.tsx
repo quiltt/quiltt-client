dfd4f60de3bdea6d6e9e083ff88ed8b125fe2481
Zubair Aziz <zubairaziz.dev@gmail.com>
2022-03-07 12:25:49 -0500

    Add PlaidDisconnect Button to exports
    Signed-off-by: Zubair Aziz <zubairaziz.dev@gmail.com>

--- src/components/Plaid/index.tsx
+++ src/components/Plaid/index.tsx
@@ -1,3 +1,4 @@
 export { default as PlaidConnectButton } from './PlaidConnectButton'
+export { default as PlaidDisconnectButton } from './PlaidDisconnectButton'
 export { default as PlaidReconnectButton } from './PlaidReconnectButton'
 export { default as PlaidStatusIndicator } from './PlaidStatusIndicator'
