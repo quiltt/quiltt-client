c4e9d3dc79f3d09cc4e75e968b9a0edff8bbff84
Zubair Aziz <zubairaziz.dev@gmail.com>
2022-03-14 04:05:12 -0400

    Update exports
    Signed-off-by: Zubair Aziz <zubairaziz.dev@gmail.com>

--- src/components/MX/index.ts
+++ src/components/MX/index.ts
@@ -1,2 +1,3 @@
 export { default as MxConnectButton } from './MxConnectButton'
+export { default as MxNewConnectionButton } from './MxNewConnectionButton'
 export { default as MxWidget } from './MxWidget'
--- src/components/Plaid/index.tsx
+++ src/components/Plaid/index.tsx
@@ -1,3 +1,4 @@
 export { default as PlaidConnectButton } from './PlaidConnectButton'
+export { default as PlaidNewConnectionButton } from './PlaidNewConnectionButton'
 export { default as PlaidReconnectButton } from './PlaidReconnectButton'
 export { default as PlaidStatusIndicator } from './PlaidStatusIndicator'
